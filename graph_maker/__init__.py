91041535a5e8ba444f102498508e40965faec33d
Philippe Schwaller <phischwaller@gmail.com>
2024-05-09 12:50:26 +0200

    pip installable version

--- graph_maker/__init__.py
+++ graph_maker/__init__.py
@@ -3,3 +3,5 @@ from .types import Node, Edge, Ontology, LLMClient, Document
 from .neo4j_graph_model import Neo4jGraphModel
 from .llm_clients.groq_client import GroqClient
 from .llm_clients.openai_client import OpenAIClient
+
+__version__ = "0.0.1"
